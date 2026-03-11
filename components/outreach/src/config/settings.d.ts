797eb01eb9d6f58601f3cc68693da6991bbc8dd6
Anthony Bull <anthony@bluxomelabs.com>
2026-03-11 09:58:03 -0700

    fix: remove generated .d.ts files from outreach component, ignore in gitignore

--- components/outreach/src/config/settings.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const SCRIPT_PROPS: {
-    readonly ANTHROPIC_API_KEY: "ANTHROPIC_API_KEY";
-    readonly OPENAI_API_KEY: "OPENAI_API_KEY";
-};
-export declare const SHEETS: {
-    readonly CONTACTS: "Contacts";
-    readonly OUTREACH: "Outreach";
-};
-//# sourceMappingURL=settings.d.ts.map
--- components/outreach/src/ui/menu.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function onOpen(): void;
-//# sourceMappingURL=menu.d.ts.map
